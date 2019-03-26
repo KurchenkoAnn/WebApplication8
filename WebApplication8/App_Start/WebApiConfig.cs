adb200af062c61b1109fabe30752bb1e7d46dafe
Anet <Anet@A33COMP14.step.itstep.rv.ua>
2019-03-26 17:16:55 +0200

    Add project files.

---
+++ WebApplication8/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication8
+{
+    /// <summary>
+    /// WebApiConfig
+    /// </summary>
+    public static class WebApiConfig
+    {
+        /// <summary>
+        /// WebAPI class-configurator
+        /// </summary>
+        /// <param name="config"></param>
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+        }
+    }
+}
---
+++ WebApplication8/Factory.cs
@@ -0,0 +1,30 @@
+namespace WebApplication8
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Factory : DbContext
+    {
+       
+        public Factory()
+            : base("name=Factory")
+        {
+            Database.SetInitializer<Factory>(new CustomInit<Factory>());
+        }
+
+        
+         public virtual DbSet<Worker> Workers { get; set; }
+    }
+
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public decimal Salary { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
